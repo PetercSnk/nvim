17ef24e318c409ccc1906a08d3bbd7d31dd72953
Peter Snook <SnookP2@cardiff.ac.uk>
2024-05-01 16:40:07 +0100

    is expand all keymap possible?

--- lua/peter/plugins/nvim-tree.lua
+++ lua/peter/plugins/nvim-tree.lua
@@ -2,6 +2,10 @@ return {
     "nvim-tree/nvim-tree.lua",
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
+        local function expand_nvim_tree()
+            local api = require("nvim-tree.api")
+            api.tree.expand_all()
+        end
         local nvimtree = require("nvim-tree")
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
@@ -30,8 +34,10 @@ return {
             }
         })
         vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-        vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
-        vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+        vim.keymap.set("n", "<leader>et", "<cmd>NvimTreeFindFile<CR>", { desc = "Toggle file explorer on current file" })
+        vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse directories" })
         vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+        vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>", { desc = "Focus on file explorer" })
+        vim.keymap.set("n", "<leader>ea", "<cmd>expand_nvim_tree<CR>", { desc = "Expand all directories" })
     end
 }
