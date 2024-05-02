042f914b1ea92b1048a016b81c25785c1a889969
Peter Snook <SnookP2@cardiff.ac.uk>
2024-05-02 16:41:45 +0100

    wont install various lsps?

---
+++ lua/peter/plugins/mason.lua
@@ -0,0 +1,36 @@
+return {
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim"
+    },
+    config = function()
+        require("mason").setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "→",
+                    package_uninstalled = "✕"
+                }
+            }
+        })
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "asm_lsp",
+                "bashls",
+                "clangd",
+                "cssls",
+                "dockerls",
+                "docker_compose_language_service",
+                "html",
+                "hls",
+                "biome",
+                "jdtls",
+                "jinja_lsp",
+                "lua_ls",
+                "matlab_ls",
+                "pyright",
+                "sqlls"
+            }
+        })
+    end        
+}
