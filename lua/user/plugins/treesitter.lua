c6bc7026b99a883553592e3738b4aeed5ca8faeb
petercsnk <peter.ch.snook@gmail.com>
2024-05-16 19:11:52 +0100

    removed matlab

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -31,7 +31,6 @@ return {
                 "csv",
                 "haskell",
                 "java",
-                "matlab",
                 "python",
                 "regex",
                 "sql",
