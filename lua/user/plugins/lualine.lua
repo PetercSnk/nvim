97b5d3ca064074d6459b8ac5735ecbf7b9824bbd
Peter Snook <SnookP2@cardiff.ac.uk>
2024-05-29 16:17:06 +0100

    added word count func

--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -2,9 +2,21 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
+        local function getWords()
+            if vim.fn.mode() == "v" or vim.fn.mode() == "V" or vim.fn.mode() == "" then
+                return vim.fn.wordcount().visual_words .. " words"
+            else
+                return vim.fn.wordcount().words .. " words"
+            end
+        end
         require("lualine").setup({
             options = {
                 theme = "everforest"
+            },
+            sections = {
+                lualine_c = {
+                    getWords
+                }
             }
         })
     end
