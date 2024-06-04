649f2a3e39dda2b6e36bf03df8f8604f7b790fe9
Peter Snook <snookp2@cardiff.ac.uk>
2024-06-04 13:40:27 +0100

    added brighter visual highlight

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -4,7 +4,10 @@ return {
     config = function()
         require("tokyonight").setup({
             style = "moon",
-            transparent = vim.g.transparent_enabled
+            transparent = vim.g.transparent_enabled,
+            on_colors = function(colors)
+                colors.bg_visual = "#FFFEE0"
+            end
         })
         vim.cmd("colorscheme tokyonight")
     end
