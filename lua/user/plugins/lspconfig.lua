6e8660e71d8703985e003eed59272638ec715a44
Peter Snook <snookp2@cardiff.ac.uk>
2024-10-14 10:04:46 +0100

    needs fix

--- lua/user/plugins/lspconfig.lua
+++ lua/user/plugins/lspconfig.lua
@@ -1,37 +1,49 @@
 return {
     "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
+    event = {
+        "BufReadPre",
+        "BufNewFile"
+    },
     dependencies = {
         "hrsh7th/cmp-nvim-lsp"
     },
     config = function()
         local lspconfig = require("lspconfig")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
-        local on_attach = function(_, bufnr)
-            local opts = { buffer = bufnr, noremap = true, silent = true }
-            opts.desc = "Show references"
-            vim.keymap.set("n", "gR", vim.lsp.buf.references, opts)
-            opts.desc = "Jump to declaration"
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-            opts.desc = "Jump to definition"
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-            opts.desc = "Show implementations"
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-            opts.desc = "Jump to type definition"
-            vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
-            opts.desc = "Smart rename"
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-            opts.desc = "Show line diagnostics"
-            vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-            opts.desc = "Jump to previous diagnostic"
-            vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-            opts.desc = "Jump to next diagnostic"
-            vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-            opts.desc = "Show documentation"
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            opts.desc = "Show signature infomation"
-            vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, opts)
-        end
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+            callback = function(ev)
+                local opts = {buffer = ev.buf, silent = true}
+                opts.desc = "Show references"
+                vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+                opts.desc = "Jump to declaration"
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                opts.desc = "Jump to definition"
+                vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+                opts.desc = "Show implementations"
+                vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+                opts.desc = "Jump to type definition"
+                vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+                opts.desc = "See available code actions"
+                vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, opts)
+                opts.desc = "Smart rename"
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                opts.desc = "Show buffer diagnostics"
+                vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+                opts.desc = "Show line diagnostics"
+                vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+                opts.desc = "Jump to previous diagnostic"
+                vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                opts.desc = "Jump to next diagnostic"
+                vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+                opts.desc = "Show documentation"
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                opts.desc = "Show signature infomation"
+                vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, opts)
+                opts.desc = "Restart LSP"
+                vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+            end
+        })
         local capabilities = cmp_nvim_lsp.default_capabilities()
         require("mason-lspconfig").setup_handlers({
             function(server_name)
